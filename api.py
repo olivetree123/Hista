23f9eacfed0b47ce99cffb2404fa831abe045354
e <olivetree123@163.com>
2018-04-06 22:47:57 +0800

    分布式存储(草稿)
    分布式存储，草稿

--- api.py
+++ api.py
@@ -2,6 +2,7 @@
 
 from flask_restful import Api
 
+from config import ROLE
 from handlers.bucket import BucketEndpoint
 from handlers.bucket_list import BucketListEndpoint
 from handlers.obj import ObjEndpoint
@@ -10,9 +11,16 @@ from handlers.obj_download import ObjDownloadEndpoint
 
 api = Api()
 
-api.add_resource(BucketEndpoint,     "/api/bucket",      endpoint="bucket")
-api.add_resource(BucketListEndpoint, "/api/bucket/list", endpoint="bucket_list")
+if ROLE == "master":
+    api.add_resource(BucketEndpoint,     "/api/bucket",      endpoint="bucket")
+    api.add_resource(BucketListEndpoint, "/api/bucket/list", endpoint="bucket_list")
+    api.add_resource(ObjEndpoint,     "/api/object",      endpoint="object")
+    api.add_resource(ObjListEndpoint, "/api/object/list", endpoint="object_list")
+    api.add_resource(ObjDownloadEndpoint, "/api/object/download", endpoint="object_download")
 
-api.add_resource(ObjEndpoint,     "/api/object",      endpoint="object")
-api.add_resource(ObjListEndpoint, "/api/object/list", endpoint="object_list")
-api.add_resource(ObjDownloadEndpoint, "/api/object/download", endpoint="object_download")
+elif ROLE == "slave":
+    # slave 需要实现存储数据的接口
+    pass
+
+else:
+    raise Exception("INVALID ROLE = {}. Role should be master or slave.".format(ROLE))
