f8081fc87a9f2a54022b5590e55922b722875886
olivetree123 <olivetree123@163.com>
2018-04-24 15:40:21 +0800

    fix bug

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ from flask import Flask
 from flask_restful import Resource
 from api import api
 
-from config import port
+from config import PORT
 from models import db
 from models.obj import Obj
 from models.host import Host
@@ -16,4 +16,4 @@ api.init_app(app)
 db.create_tables([Obj, Bucket, Host], safe=True)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=port)
+    app.run(debug=True, port=PORT)
